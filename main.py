7c7762412fb5b4465b76ab6a27ec0391a7e20d82
Ubuntu <lisalei67@gmail.com>
2018-05-31 02:13:20 +0000

    fix dataset_label option impl

--- main.py
+++ main.py
@@ -354,7 +354,8 @@ def _train():
     # Permutate train and test split (SEPARATE FOLDERS)
     index_permutation_split = random.sample(range(num_filename_train), num_filename_train)
     filenames_input_train = [filenames_input_train[x] for x in index_permutation_split]
-    if FLAGS.dataset_label != '':  
+    if FLAGS.dataset_label != FLAGS.dataset_train:  
+        print("SHOUTN!!!!!!!",FLAGS.dataset_label)
         index_permutation_split = random.sample(range(len(filenames_output_train)), num_filename_train)
     elif FLAGS.permutation_split:
         index_permutation_split = random.sample(range(num_filename_train), num_filename_train)      
