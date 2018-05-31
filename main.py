260dbfa41cb28778400b6232828d102c15232577
Ubuntu <lisalei67@gmail.com>
2018-05-31 02:17:36 +0000

    minor

--- main.py
+++ main.py
@@ -355,7 +355,6 @@ def _train():
     index_permutation_split = random.sample(range(num_filename_train), num_filename_train)
     filenames_input_train = [filenames_input_train[x] for x in index_permutation_split]
     if FLAGS.dataset_label != FLAGS.dataset_train:  
-        print("SHOUTN!!!!!!!",FLAGS.dataset_label)
         index_permutation_split = random.sample(range(len(filenames_output_train)), num_filename_train)
     elif FLAGS.permutation_split:
         index_permutation_split = random.sample(range(num_filename_train), num_filename_train)      
